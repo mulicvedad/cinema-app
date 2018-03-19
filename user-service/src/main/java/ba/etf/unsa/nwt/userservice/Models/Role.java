83af5a73592a75504a15356719f90b21c0e9af9b
EdinBegic <ebegic2@etf.unsa.ba>
2018-03-19 03:49:53 +0100

    Initial setup of user service

---
+++ user-service/src/main/java/ba/etf/unsa/nwt/userservice/Models/Role.java
@@ -0,0 +1,46 @@
+package ba.etf.unsa.nwt.userservice.Models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private Collection<User> users;
+
+    public Role() {}
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(nullable = false, length=50)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @OneToMany(mappedBy = "role")
+    @JsonIgnore
+    public Collection<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Collection<User> users) {
+        this.users = users;
+    }
+}
---
+++ user-service/src/main/java/ba/etf/unsa/nwt/userservice/Models/User.java
@@ -0,0 +1,89 @@
+package ba.etf.unsa.nwt.userservice.Models;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Role role;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String passwordHash;
+    private String email;
+
+    public User(Role role, String firstName, String lastName, String passwordHash, String email)
+    {
+        this.role = role;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.passwordHash = passwordHash;
+        this.email = email;
+    }
+    public  User() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "role_id", referencedColumnName = "id", nullable = false)
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Column(name = "first_name", nullable = false, length = 50)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    @Column(name = "last_name", nullable = false, length = 50)
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Column(nullable = false, length = 50)
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Column(name="password_hash", nullable = false, length = 2000)
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    @Column(nullable = false, length = 100)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
