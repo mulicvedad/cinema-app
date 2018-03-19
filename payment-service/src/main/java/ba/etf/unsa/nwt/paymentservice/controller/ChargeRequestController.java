aa280abe2433eef614675968d018535f5c584dd4
Adna33 <adna1karkelja@gmail.com>
2018-03-19 21:22:35 +0100

    Save entries to repo

---
+++ payment-service/src/main/java/ba/etf/unsa/nwt/paymentservice/controller/ChargeRequestController.java
@@ -0,0 +1,16 @@
+package ba.etf.unsa.nwt.paymentservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Adna Karkelja on 3/19/18.
+ *
+ * @author Adna Karkelja
+ */
+
+@RestController
+@RequestMapping("/payments")
+public class ChargeRequestController {
+
+}
