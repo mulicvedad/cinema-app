0811c20e0b7e107fbb7e4ca6d931e0f1d09c66b8
Lejla Zecevic <zeceviclejla1@gmail.com>
2018-05-13 20:49:02 +0200

    movie-service frontend

---
+++ cinema-frontend/app/services/movie-service.js
@@ -0,0 +1,44 @@
+import Ember from 'ember';
+import BaseHttpService from './base-http-service';
+
+export default BaseHttpService.extend({
+	getAllGenres() {
+		return this.ajax('GET', '/movie/movies/genres');
+	},
+
+	getMostPopularMovies() {
+		return this.ajax('GET', '/movie/movies/tmdb/popular');
+	},
+
+	getMovieByTmdbId: function(id) {
+		return this.ajax('GET', `/movie/movies/tmdb/${id}`);
+	},
+
+	getMovieByTitle: function(title) {
+		return this.ajax('GET', '/movie/movies/find/title?title' + title);
+	},
+
+	getMovieById: function(id) {
+		return this.ajax('GET', `/movie/movies/${id});
+	},		
+
+	deleteMovie: function(id) {
+		return this.ajax('DELETE', `/movie/movies/${id});
+	},
+
+	getMovieCast: function(id) {
+		return this.ajax('GET', `/movie/movies/tmdb/${id}/credits);
+	},
+
+	getReviewsByMovieId: function(id) {
+		return this.ajax('GET', `/movie/review/${id});
+	},
+
+	getReviewsByUserId: function(id) {
+		return this.ajax('GET', `/movie/review/user/${id});
+	},
+
+	createNewMovie: function(movie) {
+		return this.ajax('POST', '/movie/movies/new', movie);
+	}
+});
