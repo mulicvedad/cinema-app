cf94f7e3806238aad28c27f9a555d40a9e95039d
Adna33 <adna1karkelja@gmail.com>
2018-05-26 19:10:49 +0200

    Show title of chosen date

--- cinema-frontend/app/controllers/showing.js
+++ cinema-frontend/app/controllers/showing.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
+import moment from 'moment';
 
 export default Ember.Controller.extend({
-    session: Ember.inject.service('session')
+    session: Ember.inject.service('session'),
+    today: moment().format('YYYY-MM-DD'),
+    showingDate:null
 });
